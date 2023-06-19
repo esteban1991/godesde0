d972f6e8bc1ff0d5e296e6c8c23b9601f49ccdfe
Esteban Peralta <esteban.peralta@aerdata.com>
2023-06-19 17:18:50 +0200

    conicionales

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"github/godesde0/variables"
+	"runtime"
 )
 
 func main() {
 	estado, texto := variables.ConviertoATexto(1588)
 	fmt.Println(estado, texto)
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("esto no es windows")
+
+	} else {
+		fmt.Println("esto es windows ")
+	}
 }
