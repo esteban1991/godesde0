608a7c359a8212dc0c85ea8fe587f9889b879c09
Esteban Peralta <esteban.peralta@aerdata.com>
2023-06-21 16:58:28 +0200

    capturas pantallas 14

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"github/godesde0/ejercicios"
-	"github/godesde0/variables"
-	"runtime"
+	"github/godesde0/teclado"
 )
 
 func main() {
-	estado, texto := variables.ConviertoATexto(1588)
-	fmt.Println(estado, texto)
-	if os := runtime.GOOS; os == "linux" || os == "OS X." {
-		fmt.Println("esto no es windows")
+	// estado, texto := variables.ConviertoATexto(1588)
+	// fmt.Println(estado, texto)
+	// if os := runtime.GOOS; os == "linux" || os == "OS X." {
+	// 	fmt.Println("esto no es windows")
 
-	} else {
-		fmt.Println("esto es windows ")
-	}
+	// } else {
+	// 	fmt.Println("esto es windows ")
+	// }
 
-	num, mensaje := ejercicios.ConviertoANum("50")
-	fmt.Println(num, mensaje)
+	// num, mensaje := ejercicios.ConviertoANum("50")
+	// fmt.Println(num, mensaje)
+	teclado.IngresosNumeros()
 
 }
